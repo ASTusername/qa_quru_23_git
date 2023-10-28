b126648ae6fba4c6ad0afa28daae306d511f5e73
Александр <143889404+ASTusername@users.noreply.github.com>
2023-10-28 19:08:01 +0300

    Update Main.java

--- src/main/java/quru/qa/Main.java
+++ src/main/java/quru/qa/Main.java
@@ -2,6 +2,6 @@ package quru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi world!!!!!!!");
+        System.out.println("Hi world!");
     }
 }
